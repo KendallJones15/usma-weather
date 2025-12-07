985e753df6e7773a34c5b6fe814779fe68a5b9fa
KendallJones15 <kendall.jones@westpoint.edu>
2025-12-07 09:08:26 -0500

    Implement getWeather function to fetch weather
    Add async function to fetch weather data from API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 <script src="script.js"></script>
+
+async function getWeather() {
+  const lat = 41.3912;
+  const lon = -73.964;
+
+  const pointResponse = await fetch(`https://api.weather.gov/points/${lat},${lon}`);
+  const pointData = await pointResponse.json();
+
+  const forecastUrl = pointData.properties.forecast;
+  const forecastResponse = await fetch(forecastUrl);
+  const forecastData = await forecastResponse.json();
+
+  return forecastData.properties.periods[0];
+}
